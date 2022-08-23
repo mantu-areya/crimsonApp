2bc53228bb1f2552bb139a4155a7217e95a623d1
mahantesh_areya <mahantesh.pyati@areya.tech>
2022-08-23 10:51:40 +0530

    code changes for android number format

--- crimson/src/utilities/camera/CameraScreen.js
+++ crimson/src/utilities/camera/CameraScreen.js
@@ -1,17 +1,26 @@
-import { View,TouchableOpacity } from "react-native"
 import styled from "styled-components/native";
 import React, { useRef, useState, useEffect } from "react";
 import { Camera } from "expo-camera";
 import { Text } from "../../components/typography/text.component";
+import { SafeArea } from "../../components/utility/safe-area.component";
+import {StyleSheet, View, TouchableOpacity, Alert, ImageBackground, Image} from 'react-native'
 
 const CameraView = styled(Camera)`
   width: 100%;
   height: 100%;
 `;
 
+const CaptureButton = styled(TouchableOpacity)`
+margin-top:170%;
+margin-left:40%
+`;
+
 export const CameraScreen = () => {
   const [hasPermission, setHasPermission] = useState(null);
   const cameraRef = useRef();
+  const [startCamera, setStartCamera] = React.useState(false)
+  const [previewVisible, setPreviewVisible] = React.useState(false)
+  const [capturedImage, setCapturedImage] = React.useState(null)
 
   useEffect(() => {
     (async () => {
@@ -20,6 +29,31 @@ export const CameraScreen = () => {
     })();
   }, []);
 
+  const __startCamera = async () => {
+    const {status} = await Camera.requestCameraPermissionsAsync()
+    console.log(status)
+    if (status === 'granted') {
+      setStartCamera(true)
+    } else {
+      Alert.alert('Access denied')
+    }
+  }
+
+  const __takePicture = async () => {
+    const photo = await cameraRef.current.takePictureAsync();
+    console.log(photo)
+    setPreviewVisible(true)
+    //setStartCamera(false)
+    setCapturedImage(photo)
+  }
+
+  const __savePhoto = () => { }
+  const __retakePicture = () => {
+    setCapturedImage(null)
+    setPreviewVisible(false)
+    __startCamera()
+  }
+
   if (hasPermission === null) {
     return <View />;
   }
@@ -27,8 +61,115 @@ export const CameraScreen = () => {
     return <Text>No access to camera</Text>;
   }
   return (
-    <CameraView
-      ref={(camera) => (cameraRef.current = camera)}
-      ></CameraView>
-      );
+
+    <View
+      style={{
+        flex: 1,
+        width: '100%'
+      }}
+    >
+{previewVisible && capturedImage ? (
+            <CameraPreview photo={capturedImage} savePhoto={__savePhoto} retakePicture={__retakePicture} />
+          ) : (
+      <CameraView
+        ref={(camera) => (cameraRef.current = camera)}
+      >
+        <SafeArea>
+          <CaptureButton
+            onPress={__takePicture}
+            style={{
+              width: 70,
+              height: 70,
+              bottom: 0,
+              borderRadius: 50,
+              backgroundColor: '#fff'
+            }}
+          />
+
+        </SafeArea>
+
+      </CameraView>
+          )}
+
+
+    </View>
+  );
+}
+
+
+
+const CameraPreview = ({photo, retakePicture, savePhoto}) => {
+  console.log('sdsfds', photo)
+  return (
+    <View
+      style={{
+        backgroundColor: 'transparent',
+        flex: 1,
+        width: '100%',
+        height: '100%'
+      }}
+    >
+      <ImageBackground
+        source={{uri: photo && photo.uri}}
+        style={{
+          flex: 1
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            padding: 15,
+            justifyContent: 'flex-end'
+          }}
+        >
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between'
+            }}
+          >
+            <TouchableOpacity
+              onPress={retakePicture}
+              style={{
+                width: 130,
+                height: 40,
+
+                alignItems: 'center',
+                borderRadius: 4
+              }}
+            >
+              <Text
+                style={{
+                  color: '#fff',
+                  fontSize: 20
+                }}
+              >
+                Re-take
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={savePhoto}
+              style={{
+                width: 130,
+                height: 40,
+
+                alignItems: 'center',
+                borderRadius: 4
+              }}
+            >
+              <Text
+                style={{
+                  color: '#fff',
+                  fontSize: 20
+                }}
+              >
+                save photo
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  )
 }
