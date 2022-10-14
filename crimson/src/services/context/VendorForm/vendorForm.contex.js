2da37eec9e251553987a42f575e18d629033a40f
mahantesh_areya <mahantesh.pyati@areya.tech>
2022-10-14 15:48:28 +0530

    dateissuefix

--- crimson/src/services/context/VendorForm/vendorForm.contex.js
+++ crimson/src/services/context/VendorForm/vendorForm.contex.js
@@ -31,7 +31,7 @@ export const VendorFormContextProvider = ({ children }) => {
     let date = new Date().toLocaleDateString()
     let formatedDate = date.split('/')
   const data = [{
-      "file_name":"Contractor_Signature_"+formatedDate[2]+"-"+formatedDate[0]+"-"+formatedDate[1],
+      "file_name":"Contractor_Signature_"+new Date().toISOString().slice(0, 10),
       "image_data": img,
       "parent_record_id":inspId,
       "image_type":"sign_item",
