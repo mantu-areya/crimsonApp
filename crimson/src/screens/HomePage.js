aee958e9e84349a93f916b460a2b82d6eb5f40f9
Aman Shukla <amanks7400@gmail.com>
2022-10-31 18:33:21 +0530

    New Home Page Line Item Style

--- crimson/src/screens/HomePage.js
+++ crimson/src/screens/HomePage.js
@@ -2,8 +2,8 @@ import React, { useContext } from "react";
 import { InspectionsContext } from "../services/inspections/inspections.contex"
 
 
-import { Searchbar as PaperSearchBar, Colors, IconButton } from 'react-native-paper';
-import { ActivityIndicator, Dimensions, FlatList, Text, TouchableOpacity, View } from "react-native";
+import { Searchbar as PaperSearchBar, Colors, IconButton, Menu, Button, Card } from 'react-native-paper';
+import { ActivityIndicator, Dimensions, FlatList, Text, TextInput, TouchableOpacity, View } from "react-native";
 import styled from "styled-components/native";
 
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -11,33 +11,29 @@ import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 const backIcon = <Icon name="arrow-left" size={16} />;
 const rightArr = <Icon name="angle-right" size={48} color="white" />;
+const caretDown = <Icon name="caret-down" size={16} color="white" />;
 
 
 const TopContainer = styled.View`
-height: 180px;
-background-color:#2B243E;
+background-color:#14181B;
 width: 100%;
-border-bottom-left-radius:24px ;
-border-bottom-right-radius:24px ;
 position: relative;
-padding: 16px;
+padding: 24px 16px 12px;
 `;
 
-const BackNavigatorButton = styled.Text`
-color:white;
-font-size:18px;
-font-weight:bold;
-`;
 
 const BottomContainer = styled.View`
-padding: 8px 24px;
+padding: 12px;
 flex: 1;
 width: 100%;
+background-color: #F1F4F8;
 `;
 
 export const HomePage = ({ navigation }) => {
   const { isLoading, inspections } = useContext(InspectionsContext);
   const [searchQuery, setSearchQuery] = React.useState('');
+  const [selectedOption, setSelectedOption] = React.useState('PENDING HM REVIEW');
+  const [open, setOpen] = React.useState(false);
 
   const onChangeSearch = query => setSearchQuery(query);
 
@@ -47,17 +43,23 @@ export const HomePage = ({ navigation }) => {
     <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
       {/* Top */}
       <TopContainer>
-        {/* Back Button */}
-        <BackNavigatorButton onPress={() => navigation.goBack()}>
-          {backIcon} Back
-        </BackNavigatorButton>
-
         {/* Heading */}
-        <Text style={{ width: "100%", marginTop: 40, textAlign: "center", color: "white", fontSize: 18, fontWeight: "bold" }}>All Inspections</Text>
-
+        <Text style={{ fontSize: 24, color: "white", fontFamily: "URBAN_BOLD", marginBottom: 4 }}>Welcome!</Text>
+        {/* Menu */}
+        <Text style={{ fontSize: 18, color: "#94A1AC", fontFamily: "URBAN_MEDIUM" }} onPress={() => setOpen(!open)}>{selectedOption} {caretDown} </Text>
+        <View style={{ backgroundColor: 'white', display: open ? 'flex' : 'none', margin: 8, maxWidth: 128 }}>
+          <Menu.Item leadingIcon="redo" onPress={() => { }} title="Redo" />
+          <Menu.Item leadingIcon="undo" onPress={() => { }} title="Undo" />
+          <Menu.Item leadingIcon="content-cut" onPress={() => { }} title="Cut" />
+        </View>
         {/* Searchbar */}
-        <PaperSearchBar onChangeText={onChangeSearch}
-          value={searchQuery} style={{ borderRadius: 48, marginTop: 12 }} placeholder="Search inspections..." />
+        <View style={{ backgroundColor: '#F1F4F8', justifyContent: "space-between", alignItems: 'center', flexDirection: 'row', padding: 12, marginVertical: 8 }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Icon name="search" color="grey" size={18} />
+            <TextInput placeholder="Address, city, state.." style={{ fontFamily: "URBAN_BOLD", backgroundColor: "transparent", marginLeft: 16, fontSize: 18 }} />
+          </View>
+          <Button labelStyle={{ fontFamily: "URBAN_BOLD" }} style={{ backgroundColor: "#4B39EF", padding: 8 }} mode="contained">Search</Button>
+        </View>
 
       </TopContainer>
       {/*  Bottom */}
@@ -85,29 +87,19 @@ export const HomePage = ({ navigation }) => {
 
 function ListViewCard({ data }) {
   const navigation = useNavigation()
-  let w = Dimensions.get('window').width - 48
+  let w = Dimensions.get('window').width - 12
   const inspectionData = data.item;
 
   return (
-    <TouchableOpacity style={{ width: '100%' }} onPress={() => navigation.navigate('InspectionsDetail', { inspectionData })}>
-      <View style={{ alignItems: "center", flexDirection: "row", borderRadius: 8, width: w, marginBottom: 16, padding: 8, backgroundColor: "#6A579A" }}>
-        <View style={{ width: "80%" }}>
-          <Text style={{ color: 'white', fontFamily: 'URBAN_BOLD', fontSize: 14, marginBottom: 4 }}  >{data.item.Property_Address__c === '' ? 'Property Address NA' : data.item.Property_Address__c}</Text>
-          <Text style={{ color: 'white', fontFamily: 'URBAN_REGULAR', fontSize: 12, marginBottom: 2 }} >Repair estimator : {data.item?.Repair_Estimator__r?.Name}</Text>
-          <Text style={{ color: 'white', fontFamily: 'URBAN_REGULAR', fontSize: 12, marginBottom: 2 }} >HHM Field PM : {data.item.HHM_Field_PM__r.Name}</Text>
-          <Text style={{ color: 'white', fontFamily: 'URBAN_REGULAR', fontSize: 12, marginBottom: 2 }} >Inspection Due Date: {data.item.GC_Inspection_Due_Date__c}</Text>
-          <Text style={{ color: 'white', fontFamily: 'URBAN_REGULAR', fontSize: 12, marginBottom: 2 }} >Bid Recommendation: {data.item.HHM_Bid_Recommendation__c && `$${data.item.HHM_Bid_Recommendation__c}`}</Text>
-
+    <Card onPress={() => navigation.navigate('InspectionsDetail', { inspectionData })} style={{ display: 'flex', alignItems: "center", flexDirection: "row", borderRadius: 8, marginBottom: 16 }}>
+      <View style={{ display: 'flex', alignItems: "center", flexDirection: "row", padding: 16 }}>
+        <View>
+          <Text style={{ color: 'black', fontFamily: 'URBAN_MEDIUM', fontSize: 18, marginBottom: 4 }}  >{data.item.Property_Address__c === '' ? 'Property Address NA' : data.item.Property_Address__c}</Text>
+          <Text style={{ color: '#A6AFB9', fontFamily: 'URBAN_REGULAR', fontSize: 16, marginBottom: 2 }} >GC SUBMITTED BID : {data.item?.Repair_Estimator__r?.Name}</Text>
+          <Text style={{ color: '#A6AFB9', fontFamily: 'URBAN_REGULAR', fontSize: 16, marginBottom: 2 }} >TARGET REHAB COMPLETE DATE : {data.item?.Target_Rehab_Complete_Date__c}</Text>
         </View>
-        <View >
-          <TouchableOpacity onPress={() => navigation.navigate('InspectionsDetail', { inspectionData })} style={{ justifyContent: "center", alignItems: "center" }}>
-            {rightArr}
-          </TouchableOpacity>
-          <Text style={{ marginTop: 16, color: 'white', fontFamily: 'URBAN_REGULAR', fontSize: 10 }} >
-            {data.item.Name}
-          </Text>
-        </View>
+       
       </View>
-    </TouchableOpacity>
+    </Card>
   )
 }
