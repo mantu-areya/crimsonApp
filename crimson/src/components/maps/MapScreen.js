8f5ba70247664192ceab004aeb43273a7de405fa
mahantesh_areya <mahantesh.pyati@areya.tech>
2022-11-16 00:07:19 +0530

    code changes to navigate the mnative map

--- crimson/src/components/maps/MapScreen.js
+++ crimson/src/components/maps/MapScreen.js
@@ -2,6 +2,8 @@ import React, { useContext, useState, useEffect,useRef } from "react";
 import MapView,{Marker} from "react-native-maps";
 import styled from "styled-components/native";
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { Button,Platform } from "react-native";
+import * as Linking from 'expo-linking';
 
 const Map = styled(MapView)`
   height: 200px;
@@ -17,7 +19,34 @@ const [mocklocations,setMockLocations] = useState(null)
   const { Property_Latitude__c, Property_Longitude__c } = inspections;
 
 
+  const handleGetDirections = () => {
+    const data = {
 
+      destination: {
+        latitude: Property_Latitude__c,
+        longitude: Property_Longitude__c
+      },
+      params: [
+        {
+          key: "travelmode",
+          value: "driving"        // may be "walking", "bicycling" or "transit" as well
+        },
+        {
+          key: "dir_action",
+          value: "navigate"       // this instantly initializes navigation using the given travel mode
+        }
+      ],
+    }
+
+    Platform.select({
+      ios: () => {
+             Linking.openURL('http://maps.apple.com/maps?daddr='+Property_Latitude__c+','+Property_Longitude__c+'&dirflg=d&t=m');
+      },
+      android: () => {
+          Linking.openURL('http://maps.google.com/maps?daddr=' +Property_Latitude__c+ ',' +Property_Longitude__c);
+      }
+  })();
+  }
 
   return (
     <>
@@ -40,6 +69,7 @@ const [mocklocations,setMockLocations] = useState(null)
             >
             </Marker>
       </Map>
+      <Button onPress={()=>handleGetDirections()} title="Get Directions" />
     </>
   );
 };
